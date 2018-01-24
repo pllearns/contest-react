5eafc4229da7819ca922e9dca747f17804809d7c
pllearns <pllearns@gmail.com>
2018-01-23 23:42:22 -0800

    Contest entry updated in client

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: '',
+    message: ''
   };
 
   async componentDidMount() {
@@ -18,15 +20,46 @@ class App extends Component {
     this.setState({ manager, players, balance });
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({ message: 'waiting on successful entry!'})
+
+    const accounts = await web3.eth.getAccounts();
+    await contest.methods.enter().send({ 
+      value: web3.utils.toWei(this.state.value, 'ether'), 
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'You are entered!'})
+  };
+
   render() {
     return (
       <div> 
         <h2>Contest Contract</h2>
         <p>
           This contract is managed by {this.state.manager}
+        </p>
+        <p>
           There are currently {this.state.players.length} competing to 
           win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
-          </p>
+        </p>
+          
+          <hr />
+          <form onSubmit={this.onSubmit}>
+            <h4>Enter the contest!</h4>
+            <div>
+              <label>How much ether?</label>
+              <input 
+                value={this.state.value}
+                onChange={ event => this.setState({ value: event.target.value})}
+              />
+            </div>
+            <button>Enter!</button>
+          </form>
+          <hr />
+          <h1>{this.state.message}</h1>
       </div>
     );
   }

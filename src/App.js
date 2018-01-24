658770a7cdbe6ee804ef4d7ed3a008b9c9414807
pllearns <pllearns@gmail.com>
2018-01-23 23:21:29 -0800

    Update view with contest balance and number of players in contest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from './web3'
 import contest from './contest'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = { manager: '' }
-  }
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  };
 
   async componentDidMount() {
     const manager = await contest.methods.manager().call();
-    this.setState({ manager });
+    const players = await contest.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(contest.options.address);
+
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div> 
         <h2>Contest Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}
+          There are currently {this.state.players.length} competing to 
+          win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+          </p>
       </div>
     );
   }
